b6946ead959eca7be7d37821edc3b89a8340ff98
AbdulMajeed <chowdhury4554@gmail.com>
2023-09-30 21:01:17 -0400

    app user settings, create, write, save, and reset. prayers auto populate

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using AdhanyDesktop.Model;
+using System.Diagnostics.Metrics;
 using System.IO;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -22,7 +23,55 @@ namespace AdhanyDesktop
 
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        /* 
+         * This method is called when the form is loaded.
+         * Populates the method combo box with the available methods.
+         */
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var methods = new List<CalculationMethod>();
+
+            // Create method objects and add them to the methods list
+            foreach (var kvp in CalculationMethod.methodDictionary)
+            {
+                methods.Add(new CalculationMethod { id = kvp.Key, name = kvp.Value });
+            }
+            // Bind the list to the combo box
+            ddl_method.DataSource = methods;
+            ddl_method.DisplayMember = "name";
+            ddl_method.ValueMember = "id";
+
+            // Check for saved settings
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.Country) || !String.IsNullOrEmpty(Properties.Settings.Default.City))
+            {
+                // populate the combo boxes
+                ddl_country.SelectedItem = Properties.Settings.Default.Country;
+                ddl_city.SelectedItem = Properties.Settings.Default.City;
+                ddl_method.SelectedValue = Properties.Settings.Default.Method;
+
+                statusProgressBar.Value = 25;
+                // get the prayer times for these settings
+                PrayerTimesAPI prayerTimes = await GetPrayerTimesAsync(
+                    Properties.Settings.Default.Country,
+                    Properties.Settings.Default.City,
+                    Properties.Settings.Default.Method);
+
+                // Populate the table with the prayer times
+                statusProgressBar.Value = 50;
+                res_location.Text = Properties.Settings.Default.City + ", " + Properties.Settings.Default.Country;
+                displayPrayerTimes(prayerTimes);
+                statusProgressBar.Value = 100;
+
+                statusLabel.Text = "Prayer times from saved settings";
+            }
+            else
+            {
+                statusLabel.Text = "Please select a country and city";
+
+            }
+        }
+
+        private async void btn_save_Click(object sender, EventArgs e)
         {
             statusProgressBar.Value = 0;
             table_fetchedData.Visible = false;
@@ -33,48 +82,35 @@ namespace AdhanyDesktop
             var method = (CalculationMethod)ddl_method.SelectedItem;
 
             statusProgressBar.Value = 25;
-
-            PrayerTimesAPI prayerTimes = await GetPrayerTimesAsync(country, city, method.id);
+            // Save the settings
+            Properties.Settings.Default.Country = country;
+            Properties.Settings.Default.City = city;
+            Properties.Settings.Default.Method = method.id;
+            Properties.Settings.Default.Save();
 
             statusProgressBar.Value = 50;
-
-            res_location.Text = city + ", " + country;
-
-            res_date.Text = prayerTimes.Data.Date.readable;
-            res_hijri.Text = prayerTimes.Data.Date.hijri.date;
-            res_fajr.Text = prayerTimes.Data.Timings.Fajr;
-            res_sunrise.Text = prayerTimes.Data.Timings.Sunrise;
-            res_dhuhr.Text = prayerTimes.Data.Timings.Dhuhr;
-            res_asir.Text = prayerTimes.Data.Timings.Asr;
-            res_maghrib.Text = prayerTimes.Data.Timings.Maghrib;
-            res_isha.Text = prayerTimes.Data.Timings.Isha;
+            PrayerTimesAPI prayerTimes = await GetPrayerTimesAsync(country, city, method.id);
 
             statusProgressBar.Value = 75;
+            res_location.Text = city + ", " + country;
+            displayPrayerTimes(prayerTimes);
 
-            table_fetchedData.Visible = true;
 
             statusProgressBar.Value = 100;
             statusLabel.Text = "Done";
             MessageBox.Show("Prayer times fetched successfully");
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            var methods = new List<CalculationMethod>();
 
-            // Create method objects and add them to the methods list
-            foreach (var kvp in CalculationMethod.methodDictionary)
-            {
-                methods.Add(new CalculationMethod { id = kvp.Key, name = kvp.Value });
-            }
-
-            // Bind the list to the combo box
-            ddl_method.DataSource = methods;
-            ddl_method.DisplayMember = "name";
-            ddl_method.ValueMember = "id";
-        }
-
-
+        /// <summary>
+        /// This fetches the prayer times from the API using the given parameters, and returns a PrayerTimesAPI object.
+        /// If the data has been fetched recently, it will be returned from the cache.
+        /// the method parameter is the calculation method index number <see cref="https://aladhan.com/calculation-methods"/>
+        /// </summary>
+        /// <param name="country">Country Name or Code</param>
+        /// <param name="city">City Name</param>
+        /// <param name="method">The Method index number</param>
+        /// <returns></returns>
         private async Task<PrayerTimesAPI> GetPrayerTimesAsync(string country, string city, int method)
         {
             PrayerTimesAPI prayerTimes = null;
@@ -93,6 +129,38 @@ namespace AdhanyDesktop
                 Console.WriteLine(e.Message);
             }
             return prayerTimes;
+        }
+
+        /// <summary>
+        /// This is called to display the prayer times in the table from the PrayerTimesAPI object
+        /// </summary>
+        /// <param name="prayerTimes">The PrayerTimesAPI object to display</param>
+        private void displayPrayerTimes(PrayerTimesAPI prayerTimes)
+        {
+            res_date.Text = prayerTimes.Data.Date.readable;
+            res_hijri.Text = prayerTimes.Data.Date.hijri.date;
+            res_fajr.Text = prayerTimes.Data.Timings.Fajr;
+            res_sunrise.Text = prayerTimes.Data.Timings.Sunrise;
+            res_dhuhr.Text = prayerTimes.Data.Timings.Dhuhr;
+            res_asir.Text = prayerTimes.Data.Timings.Asr;
+            res_maghrib.Text = prayerTimes.Data.Timings.Maghrib;
+            res_isha.Text = prayerTimes.Data.Timings.Isha;
+
+            table_fetchedData.Visible = true;
+        }
+
+        private void resetSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reset();
+            ddl_country.Text = String.Empty;
+            ddl_city.Text = String.Empty;
+            ddl_method.Text = String.Empty;
+            MessageBox.Show("Settings reset successfully");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
